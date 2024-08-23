7874435650ac97f259e50b92a49ed9898f51d9fe
BaradTest <muhammedkmailbarad@gmail.com>
2024-08-23 11:34:18 -0400

    Added The Main Project Folders And Change The Structure Of Folders

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Added Test Text */}
+      <h1>Hi This Is v1.0 Of Barad-Repo Project</h1>
     </div>
   );
 }
